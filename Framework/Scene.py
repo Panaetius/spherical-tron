001b40c5b694bec6eef8c8ff17661d1c84268328
Ralf Grubenmann <ralf.grubenmann+github@gmail.com>
2017-01-11 20:43:16 +0100

    Adds keyboard controls

--- Framework/Scene.py
+++ Framework/Scene.py
@@ -6,7 +6,8 @@ class Scene(object):
     def __init__(self):
         self.gameObjects = []
         bikeModel = Model.Model('Assets/Models/bike.obj')
-        self.addGameObject(GameObject.GameObject(bikeModel, position = [1,0,0], xRotation=10, yRotation=30, zRotation=60))
+        self.bikeObject = GameObject.GameObject(bikeModel, position = [1,0,0], xRotation=10, yRotation=30, zRotation=60)
+        self.addGameObject(self.bikeObject)
 
     def addGameObject(self, gameObject):
         self.gameObjects.append(gameObject)
@@ -17,5 +18,24 @@ class Scene(object):
 
     def update(self):
         #update the scene
-        for gameObject in self.gameObjects:
-            gameObject.position = map(add,gameObject.position,[0.1, 0, 0])
+
+        return
+
+    def keyboard(self, ch, x, y):
+        return
+
+    def keyboard_special(self, ch, x, y):
+        if ch == 100:
+            #Left
+            self.bikeObject.position = map(add, self.bikeObject.position, [-0.1, 0, 0])
+        elif ch == 102:
+            #right
+            self.bikeObject.position = map(add, self.bikeObject.position, [0.1, 0, 0])
+        elif ch == 101:
+            #up
+            self.bikeObject.position = map(add, self.bikeObject.position, [0, 0.1, 0])
+        elif ch == 103:
+            #down
+            self.bikeObject.position = map(add, self.bikeObject.position, [0, -0.1, 0])
+
+        return
--- main.py
+++ main.py
@@ -12,54 +12,58 @@ name = 'spherical tron'
 
 scene = ''
 
+
 def main():
     global scene
-    #initialize glut window
+    # initialize glut window
     glutInit(sys.argv)
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
-    glutInitWindowSize(1024,768)
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH | GLUT_MULTISAMPLE)
+    glutInitWindowSize(1024, 768)
     glutCreateWindow(name)
 
-    #set shader and defaults
-    glClearColor(0.,0.,0.,1.)
+    # set shader and defaults
+    glClearColor(0., 0., 0., 1.)
     glShadeModel(GL_SMOOTH)
     glEnable(GL_CULL_FACE)
     glEnable(GL_DEPTH_TEST)
     glEnable(GL_LIGHTING)
 
-    #add light
-    lightZeroPosition = [10.,4.,10.,1.]
-    lightZeroColor = [0.8,1.0,0.8,1.0] #green tinged
+    # add light
+    lightZeroPosition = [10., 4., 10., 1.]
+    lightZeroColor = [0.8, 1.0, 0.8, 1.0]  # green tinged
     glLightfv(GL_LIGHT0, GL_POSITION, lightZeroPosition)
     glLightfv(GL_LIGHT0, GL_DIFFUSE, lightZeroColor)
     glLightf(GL_LIGHT0, GL_CONSTANT_ATTENUATION, 0.1)
     glLightf(GL_LIGHT0, GL_LINEAR_ATTENUATION, 0.05)
     glEnable(GL_LIGHT0)
 
-    #set glut functions
+    # set glut functions
     glutDisplayFunc(display)
     glutIdleFunc(gameloop)
+    glutKeyboardFunc(keyboard)
+    glutSpecialFunc(keyboard_special)
 
-    #set camera projection, position and direction
+    # set camera projection, position and direction
     glMatrixMode(GL_PROJECTION)
-    gluPerspective(40.,1.,1.,40.)
+    gluPerspective(40., 1., 1., 40.)
     glMatrixMode(GL_MODELVIEW)
-    gluLookAt(0,0,30,
-              0,0,0,
-              0,1,0)
+    gluLookAt(0, 0, 30,
+              0, 0, 0,
+              0, 1, 0)
     glPushMatrix()
 
-    #init scene
+    # init scene
     scene = Scene.Scene()
 
     glutMainLoop()
     return
 
+
 def display():
-    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glPushMatrix()
-    color = [1.0,0.,0.,1.]
-    glMaterialfv(GL_FRONT,GL_DIFFUSE,color)
+    color = [1.0, 0., 0., 1.]
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, color)
 
     scene.render()
 
@@ -67,11 +71,27 @@ def display():
     glutSwapBuffers()
     return
 
+
 def gameloop():
-    #main game loop, called repeatedly
+    # main game loop, called repeatedly
     global scene
     scene.update()
     glutPostRedisplay()
     return
+
+
+def keyboard(ch, x, y):
+    global scene
+    scene.keyboard(ch, x, y)
+    glutPostRedisplay()
+    return 0
+
+
+def keyboard_special(ch, x, y):
+    global scene
+    scene.keyboard_special(ch, x, y)
+    glutPostRedisplay()
+    return 0
+
 
 if __name__ == '__main__': main()
