83d56865e03aabe45a1c26956b05e752fafb65d0
Samuel Kupferschmid <samuel.kupferschmid@gmail.com>
2017-01-16 07:17:29 +0100

    modified Bot

--- Framework/GameObjects/AIPlayer.py
+++ Framework/GameObjects/AIPlayer.py
@@ -24,77 +24,19 @@ class AIPlayer(Bike):
         self.speed = 0.03
 
    def update(self, deltaTime, camera, enemy):
-        trail = self.trail + enemy.trail
-        trailFront = False
-        collisionSphereRadius = 8
 
-        # check if collision is near
-        for i in range(0, len(trail) - 1, 1):
-            first = trail[i]
-            second = trail[i + 1]
-            point = first[0]
+        v1 = np.cross(self.direction, self.position)
+        v1 = v1 / np.linalg.norm(v1)
 
-            if np.linalg.norm(point - self.position) > collisionSphereRadius * 5:
-                continue #don't do any collision checks for walls too far away
+        v2 = np.cross(enemy.position, self.position)
+        v2 = v2 / np.linalg.norm(v2)
+        dot = np.dot(v1,v2)
+            
 
-            v1 = first[1] - first[0]
-            v2 = second[0] - first[0]
-            normal = np.cross(v1, v2)
-            normal = normal/np.linalg.norm(normal)
-            distance = abs(np.dot(self.collisionSphereCenter - point, normal))
-
-            if distance > 10:
-                continue #too far away
-
-            # check if we move through plane between frames
-            pos = np.array(self.position)
-            prev_pos = np.array(self.previousPosition)
-
-            #raise pos up a bit since it's a the bottom of the model, so curvature means a direct line would go below the plane
-            pos = pos + 0.5 * pos/np.linalg.norm(pos)
-            prev_pos = prev_pos + 0.5 * prev_pos / np.linalg.norm(prev_pos)
-            movement_ray = pos - prev_pos
-            mov_len = np.linalg.norm(movement_ray)
-            movement_ray = movement_ray / mov_len
-            den = np.dot(normal, movement_ray)
-            if abs(den) < 1e-6:
-                continue  # parallel
-
-            p0 = point - np.array(prev_pos)
-            t = np.dot(p0, normal) / den
-            if t > 1e-11 and t < mov_len:
-                intersect = prev_pos + t * movement_ray
-                i = intersect - point
-                l1 = np.linalg.norm(v1)
-                l2 = np.linalg.norm(v2)
-                d1 = np.dot(i, v1/l1)
-                d2 = np.dot(i, v2/l2)
-                if d1 > 0 and d1 < l1  and d2 > 0 and d2 < l2:
-                    trailFront = True
-                    break
-
-            if distance > collisionSphereRadius:
-                continue
-
-            #check if the sphere is actually within the rectangle
-            intersect = self.collisionSphereCenter - distance * normal
-            t = intersect - point
-            l1 = np.linalg.norm(v1)
-            l2 = np.linalg.norm(v2)
-            d1 = np.dot(t, v1/l1)
-            d2 = np.dot(t, v2/l2)
-
-            if d1 > -distance and d1 < l1 + distance and d2 > -distance and d2 < l2 + distance:
-                trailFront = True
-                break
-                
-
-        rand = np.random.rand()
-
-        if trailFront:
+        if dot < -0.6:
             self.direction = -np.cross(self.direction, self.position)
-        #elif rand > 0.98:
-        #    self.direction = np.cross(self.direction, self.position)
+        elif dot > 0.6:
+            self.direction = np.cross(self.direction, self.position)
 
         Bike.update(self, deltaTime, camera, enemy)
 
